e7680509282895f2df12d2f14b0443dae196947a
VaJau2 <vadim206-1998@yandex.ru>
2025-06-19 21:24:13 +0300

    carrot plant

---
+++ assets/scripts/props/carrotPlant.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+const SPAWN_TIME_MIN: float = 100
+const SPAWN_TIME_MAX: float = 150
+
+@onready var interaction_controller: InteractionController = G.player.interaction_controller
+@export var sprite: Sprite2D
+@export var code: String
+
+var has_item: bool = true
+var spawn_timer: float
+
+
+func _ready() -> void:
+	set_process(false)
+
+
+func _process(delta: float) -> void:
+	if spawn_timer > 0:
+		spawn_timer -= delta
+	else:
+		sprite.visible = true
+		has_item = true
+		set_process(false)
+
+
+func on_mouse_entered() -> void:
+	if !has_item or interaction_controller.holding_item != null: return
+	interaction_controller.show_hint.emit(code)
+
+
+func on_mouse_exited() -> void:
+	interaction_controller.hide_item_hint.emit()
+
+
+func interact() -> void:
+	if !has_item or interaction_controller.holding_item != null: return
+	
+	var item = ItemSpawner.spawn_item(code, global_position, get_parent())
+	item.visible = false
+	item.process_mode = Node.PROCESS_MODE_DISABLED
+	interaction_controller.update_holding_item(item)
+	
+	sprite.visible = false
+	has_item = false
+	spawn_timer = randf_range(SPAWN_TIME_MIN, SPAWN_TIME_MAX)
+	set_process(true)
