bc284ab101d2543d1924ddd89ba07f85c2300f28
McFord <McFord@ukr.net>
2025-07-07 00:54:27 +0300

    Add radio menu
    > Little update of HUD-design

--- assets/scripts/interface/daysLeft.gd
+++ assets/scripts/interface/daysLeft.gd
@@ -1,10 +1,10 @@
 extends Label
 
-
 func _ready() -> void:
+	G.lang_changed.connect(_on_day_tick)
 	G.time.day_tick.connect(_on_day_tick)
 	_on_day_tick()
 
 
 func _on_day_tick() -> void:
-	text = str(G.DAYS_GOAL - G.time.day)
+	text = Loc.trans("interface.money.days_left") + " " + str(G.DAYS_GOAL - G.time.day)
